43126de53990d61f23b2e1deda37abecf4edaab2
wl王立 <aywzcmq64@163.com>
2024-06-08 15:41:44 +0800

    xxx

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0      );
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank"   >
+        <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
